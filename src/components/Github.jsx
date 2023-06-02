10adcff3cb5ae4d1f7351b37bf3e4521d2499b80
SabirAnsari1 <mrr.ansari99@gmail.com>
2023-06-02 14:47:50 +0530

    portfolio

--- src/components/Github.jsx
+++ src/components/Github.jsx
@@ -1,7 +1,17 @@
 import React from "react";
-import { Box, Flex, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  Text,
+  Image,
+  useColorMode,
+  Center,
+  VStack,
+} from "@chakra-ui/react";
 
 export const Github = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+
   return (
     <Box p={"3rem"}>
       <Flex>
@@ -19,6 +29,55 @@ export const Github = () => {
           Stats
         </Text>
       </Flex>
+
+      {colorMode === "light" ? (
+        <VStack spacing={"1rem"}>
+          <Center>
+            <Image
+              src={
+                "https://github-readme-stats.vercel.app/api/top-langs/?username=sabiransari1&theme=flag-india"
+              }
+            />
+          </Center>
+
+          <Flex gap={"1rem"}>
+            <Image
+              src={
+                "https://github-readme-stats.vercel.app/api?username=sabiransari1&show_icons=true&theme=flag-india"
+              }
+            />
+            <Image
+              src={
+                "https://github-readme-streak-stats.herokuapp.com/?user=sabiransari1&theme=flag-india"
+              }
+            />
+          </Flex>
+        </VStack>
+      ) : (
+        <VStack gap={"1rem"}>
+          <Center>
+            <Image
+              src={
+                "https://github-readme-stats.vercel.app/api/top-langs/?username=sabiransari1&theme=great-gatsby"
+              }
+            />
+          </Center>
+
+          <Flex gap={"1rem"}>
+            {" "}
+            <Image
+              src={
+                "https://github-readme-stats.vercel.app/api?username=sabiransari1&show_icons=true&theme=great-gatsby"
+              }
+            />
+            <Image
+              src={
+                "https://github-readme-streak-stats.herokuapp.com/?user=sabiransari1&theme=great-gatsby"
+              }
+            />
+          </Flex>
+        </VStack>
+      )}
     </Box>
   );
 };
