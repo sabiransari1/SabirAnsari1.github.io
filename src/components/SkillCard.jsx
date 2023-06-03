67c5c363bc8899e0c1c1498da3cfac678d214227
SabirAnsari1 <mrr.ansari99@gmail.com>
2023-06-03 19:13:11 +0530

    portfolio

---
+++ src/components/SkillCard.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Box, Image, Center } from "@chakra-ui/react";
+
+export const SkillCard = ({ img, name }) => {
+  return (
+    <Box
+      className="skills-card"
+      borderRadius={"10px"}
+      cursor={"pointer"}
+      p={".5rem"}
+      boxShadow={
+        "rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px"
+      }
+      w={"120px"}
+    >
+      <Image className="skills-card-img" src={img} alt="Skill card img" />
+      <Center className="skills-card-name" mt={".5rem"}>
+        {name}
+      </Center>
+    </Box>
+  );
+};
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -11,8 +11,9 @@ import nodejs from "../assets/images/nodejs.png";
 import express from "../assets/images/express.png";
 import mongodb from "../assets/images/mongodb.png";
 import git from "../assets/images/git.png";
+import { SkillCard } from "./SkillCard";
 
-const skills = [
+const frontend = [
   { img: html, name: "HTML" },
   { img: css, name: "CSS" },
   { img: javascript, name: "JavaScript" },
@@ -20,11 +21,13 @@ const skills = [
   { img: redux, name: "Redux" },
   { img: typescript, name: "TypeScript" },
   { img: chakra, name: "Chakra-UI" },
+];
+const backend = [
   { img: nodejs, name: "Node.js" },
   { img: express, name: "Express" },
   { img: mongodb, name: "MongoDB" },
-  { img: git, name: "Git" },
 ];
+const other = [{ img: git, name: "Git" }];
 
 export const Skills = () => {
   return (
@@ -76,12 +79,12 @@ export const Skills = () => {
         {/* second */}
         <Grid
           templateColumns={{
-            base: "repeat(2,1fr)",
-            sm: "repeat(4,1fr)",
-            md: "repeat(4,1fr)",
-            lg: "repeat(5,1fr)",
-            xl: "repeat(5,1fr)",
-            "2xl": "repeat(5,1fr)",
+            base: "repeat(1,1fr)",
+            sm: "repeat(2,1fr)",
+            md: "repeat(2,1fr)",
+            lg: "repeat(2,1fr)",
+            xl: "repeat(2,1fr)",
+            "2xl": "repeat(2,1fr)",
           }}
           gap={"2rem"}
           p={{
@@ -93,41 +96,217 @@ export const Skills = () => {
             "2xl": "0 5rem",
           }}
         >
-          {skills?.map((el, index) => (
-            <Box
-              className="skills-card"
-              key={index}
-              p={".5rem"}
-              boxShadow={
-                "rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px"
-              }
-              borderRadius={"10px"}
-              cursor={"pointer"}
+          <Box>
+            <Text
+              fontSize={{
+                base: "3xl",
+                sm: "3xl",
+                md: "4xl",
+                lg: "4xl",
+                xl: "4xl",
+                "2xl": "4xl",
+              }}
+              fontWeight={"bold"}
+              bgGradient="linear-gradient(180deg, #fdc50f 26.71%, #fb982f 99.36%)"
+              bgClip="text"
             >
-              {index === 6 || index === 8 || index === 9 ? (
-                <Image
-                  className="skills-card-img"
-                  src={el.img}
-                  alt="Skill card img"
-                  borderRadius={"50%"}
-                  boxShadow={
-                    "rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px"
-                  }
-                />
-              ) : (
-                <Image
-                  className="skills-card-img"
-                  src={el.img}
-                  alt="Skill card img"
-                />
-              )}
-              <Center className="skills-card-name" mt={".5rem"}>
-                {el.name}
-              </Center>
+              Frontend
+            </Text>
+
+            <Grid
+              templateColumns={{
+                base: "repeat(2,1fr)",
+                sm: "repeat(2,1fr)",
+                md: "repeat(2,1fr)",
+                lg: "repeat(3,1fr)",
+                xl: "repeat(4,1fr)",
+                "2xl": "repeat(5,1fr)",
+              }}
+              gap={"1rem"}
+            >
+              {frontend?.map((el, index) => (
+                <SkillCard key={index} {...el} />
+              ))}
+            </Grid>
+          </Box>
+
+          <Box>
+            <Box>
+              <Text
+                fontSize={{
+                  base: "3xl",
+                  sm: "3xl",
+                  md: "4xl",
+                  lg: "4xl",
+                  xl: "4xl",
+                  "2xl": "4xl",
+                }}
+                fontWeight={"bold"}
+                bgGradient="linear-gradient(180deg, #fdc50f 26.71%, #fb982f 99.36%)"
+                bgClip="text"
+              >
+                Backend
+              </Text>
+
+              <Grid
+                templateColumns={{
+                  base: "repeat(2,1fr)",
+                  sm: "repeat(1,1fr)",
+                  md: "repeat(2,1fr)",
+                  lg: "repeat(2,1fr)",
+                  xl: "repeat(4,1fr)",
+                  "2xl": "repeat(5,1fr)",
+                }}
+                gap={"1rem"}
+              >
+                {backend?.map((el, index) => (
+                  <SkillCard key={index} {...el} />
+                ))}
+              </Grid>
             </Box>
-          ))}
+
+            <Box>
+              <Text
+                fontSize={{
+                  base: "3xl",
+                  sm: "3xl",
+                  md: "4xl",
+                  lg: "4xl",
+                  xl: "4xl",
+                  "2xl": "4xl",
+                }}
+                fontWeight={"bold"}
+                bgGradient="linear-gradient(180deg, #fdc50f 26.71%, #fb982f 99.36%)"
+                bgClip="text"
+              >
+                Other
+              </Text>
+
+              <Grid
+                templateColumns={{
+                  base: "repeat(2,1fr)",
+                  sm: "repeat(1,1fr)",
+                  md: "repeat(2,1fr)",
+                  lg: "repeat(2,1fr)",
+                  xl: "repeat(4,1fr)",
+                  "2xl": "repeat(5,1fr)",
+                }}
+                gap={"1rem"}
+              >
+                {other?.map((el, index) => (
+                  <SkillCard key={index} {...el} />
+                ))}
+              </Grid>
+            </Box>
+          </Box>
         </Grid>
       </Box>
     </Box>
   );
 };
+
+// export const Skills = () => {
+//   return (
+//     <Box
+//       p={{
+//         base: "1rem 1rem",
+//         sm: "1rem 1rem",
+//         md: "1rem 2rem",
+//         lg: "3rem 5rem",
+//         xl: "3rem 5rem",
+//         "2xl": "3rem 5rem",
+//       }}
+//       id={"skills"}
+//     >
+//       {/* first */}
+//       <Box>
+//         <Flex>
+//           <Text
+//             fontSize={{
+//               base: "3xl",
+//               sm: "3xl",
+//               md: "4xl",
+//               lg: "4xl",
+//               xl: "4xl",
+//               "2xl": "4xl",
+//             }}
+//             fontWeight={"bold"}
+//             mr={".6rem"}
+//           >
+//             My
+//           </Text>
+//           <Text
+//             fontSize={{
+//               base: "3xl",
+//               sm: "3xl",
+//               md: "4xl",
+//               lg: "4xl",
+//               xl: "4xl",
+//               "2xl": "4xl",
+//             }}
+//             fontWeight={"bold"}
+//             bgGradient="linear-gradient(180deg, #fdc50f 26.71%, #fb982f 99.36%)"
+//             bgClip="text"
+//           >
+//             Technical Skills
+//           </Text>
+//         </Flex>
+
+//         {/* second */}
+//         <Grid
+//           templateColumns={{
+//             base: "repeat(2,1fr)",
+//             sm: "repeat(4,1fr)",
+//             md: "repeat(4,1fr)",
+//             lg: "repeat(5,1fr)",
+//             xl: "repeat(5,1fr)",
+//             "2xl": "repeat(5,1fr)",
+//           }}
+//           gap={"2rem"}
+//           p={{
+//             base: "0 1rem",
+//             sm: "0 1rem",
+//             md: "0 2rem",
+//             lg: "0 4rem",
+//             xl: "0 5rem",
+//             "2xl": "0 5rem",
+//           }}
+//         >
+//           {skills?.map((el, index) => (
+//             <Box
+//               className="skills-card"
+//               key={index}
+//               p={".5rem"}
+//               boxShadow={
+//                 "rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px"
+//               }
+//               borderRadius={"10px"}
+//               cursor={"pointer"}
+//             >
+//               {index === 6 || index === 8 || index === 9 ? (
+//                 <Image
+//                   className="skills-card-img"
+//                   src={el.img}
+//                   alt="Skill card img"
+//                   borderRadius={"50%"}
+//                   boxShadow={
+//                     "rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px"
+//                   }
+//                 />
+//               ) : (
+//                 <Image
+//                   className="skills-card-img"
+//                   src={el.img}
+//                   alt="Skill card img"
+//                 />
+//               )}
+//               <Center className="skills-card-name" mt={".5rem"}>
+//                 {el.name}
+//               </Center>
+//             </Box>
+//           ))}
+//         </Grid>
+//       </Box>
+//     </Box>
+//   );
+// };
