59ae5c663e4a101bb251cdae6df7912c1bfdbc1e
SabirAnsari1 <mrr.ansari99@gmail.com>
2023-07-04 16:07:00 +0530

    updates

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,10 @@ import App from "./App";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <ChakraProvider>
-    <ColorModeScript initialColorMode="light" />
-    <App />
-  </ChakraProvider>
+  <React.StrictMode>
+    <ChakraProvider>
+      <ColorModeScript initialColorMode="light" />
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>
 );
