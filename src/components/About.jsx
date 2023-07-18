33c71390f28a3523455be4f91129ed81b334b567
sabiransari1 <mrr.ansari99@gmail.com>
2023-07-18 20:45:26 +0530

    updates

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,13 @@
 import React from "react";
-import { Flex, Box, ListItem, UnorderedList, Image, Text, Center } from "@chakra-ui/react";
+import {
+  Flex,
+  Box,
+  ListItem,
+  UnorderedList,
+  Image,
+  Text,
+  Center,
+} from "@chakra-ui/react";
 import SabirAnsari from "../assets/images/SabirAnsari.gif";
 import { Typewriter } from "react-simple-typewriter";
 
@@ -136,28 +144,31 @@ export const About = () => {
           color={"#788097"}
         >
           <ListItem>
-            Hello! My name Sabir Ansari and I enjoy creating things that live on the internet.
+            Hello! My name Sabir Ansari and I enjoy creating things that live on
+            the internet.
           </ListItem>
 
           <ListItem>
-            I'm a passionate Developer, with strong administrative and communication skills, good
-            attention to detail and the ability to write efficient code using MERN Stack.
+            I'm a passionate Developer, with strong administrative and
+            communication skills, good attention to detail and the ability to
+            write efficient code using MERN Stack.
           </ListItem>
 
           <ListItem>
-            My interest in web development started back in 2016 when I was in class 12th I saw my
-            friend making some basic webpages and I got facinated with it.
+            My interest in web development started back in 2022 when I saw my
+            friend making webpages and I got facinated with it.
           </ListItem>
 
           <ListItem>
-            As I grow and flourish as a Developer, one thing which keeps me going is my
-            inquisitiveness for discovering new things every day.
+            As I grow and flourish as a Developer, one thing which keeps me
+            going is my inquisitiveness for discovering new things every day.
           </ListItem>
 
           <ListItem>
-            Fast Forwarding to today, I built a number of web applications and 4 major projects.
-            Learned a great deal about teamwork, leadership, and communication. After months of
-            rigorous training, here I am looking for an opportunity as a full stack web developer.
+            Fast Forwarding to today, I built a number of web applications and 4
+            major projects. Learned a great deal about teamwork, leadership, and
+            communication. After months of rigorous training, here I am looking
+            for an opportunity as a full stack web developer.
           </ListItem>
         </UnorderedList>
       </Box>
